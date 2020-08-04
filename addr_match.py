4b323e5e4c4dbf70b9904d0912789d37536ffc4e
arunptl100 <arunptl100@hotmail.com>
2020-08-04 18:27:49 +0100

    made some progress

--- addr_match.py
+++ addr_match.py
@@ -2,11 +2,32 @@ from fuzzywuzzy import fuzz
 from openpyxl import load_workbook
 
 
-# parse the xlsx dataset using openpyxl
-dataset = load_workbook('resouces/Sample_Address_Data.xlsx')
-sheet = dataset.active
-row_count = sheet.max_row
-for i in range(row_count):
-   print(sheet.cell(row=i, column=2).value)
+# parse the xlsx dataset using openpyxl into lists
+sa1_l = []
+sa2_l = []
+dataset = load_workbook('resources/Sample_Address_Data.xlsx')
+worksheet = dataset.active
+# iterate over Sample Address 1 addresses
+for col_cells_sa1 in worksheet.iter_cols(min_col=1, max_col=1, min_row=2):
+    for cell_sa1 in col_cells_sa1:
+        sa1_l.append(cell_sa1.value.upper())
+# iterate over Sample Address 2 addresses
+for col_cells_sa2 in worksheet.iter_cols(min_col=2, max_col=2, min_row=2):
+    for cell_sa2 in col_cells_sa2:
+        sa2_l.append(cell_sa2.value.upper())
 
-# print(fuzz.ratio('geeksforgeeks', 'geeksgeeks'))
+# find matching addresses from each list to find matches
+matched_addr = []
+for addr_1 in sa1_l:
+    for addr_2 in sa2_l:
+        f_ratio = fuzz.ratio(addr_1, addr_2)
+        if f_ratio >= 83:
+            print("Found a potential match\n  ", addr_1, "||", addr_2, "ratio: ", f_ratio)
+            matched_addr.append(addr_1)
+            matched_addr.append(addr_2)
+
+# determine number of address before matching
+print("# Addresses:", len(sa1_l) + len(sa2_l))
+# determine number of unmatched addresses
+print("matched", len(matched_addr), "addresses")
+print("# unmatched addresses:", (len(sa1_l) + len(sa2_l)) - len(matched_addr))
