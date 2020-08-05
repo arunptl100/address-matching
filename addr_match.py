d7874226b623e3b3c3a5b8d866f01b7c027fb90e
arunptl100 <arunptl100@hotmail.com>
2020-08-05 16:27:09 +0100

    fixed an issue where incorrect house/flat/apartment numbers would match

--- addr_match.py
+++ addr_match.py
@@ -1,6 +1,11 @@
 from fuzzywuzzy import fuzz
 from openpyxl import load_workbook
 
+# benchmark
+# # Σ Addresses: 3046
+# matched 1554 addresses
+# # Σ unmatched addresses: 1492
+
 
 # class representing an address and its fuzzy matching ratio
 # Intended purpose: for every address, store a list of addr_match objects
@@ -10,7 +15,6 @@ class addr_match:
         self.addr = addr
         self.f_ratio = f_ratio
         self.f_tkn_ratio = f_tkn_ratio
-
 
 
 # parse the xlsx dataset using openpyxl into lists
@@ -23,11 +27,11 @@ worksheet = dataset.active
 # iterate over Sample Address 1 addresses
 for col_cells_sa1 in worksheet.iter_cols(min_col=1, max_col=1, min_row=2):
     for cell_sa1 in col_cells_sa1:
-        sa1_l.append(cell_sa1.value.upper())
+        sa1_l.append(cell_sa1.value)
 # iterate over Sample Address 2 addresses
 for col_cells_sa2 in worksheet.iter_cols(min_col=2, max_col=2, min_row=2):
     for cell_sa2 in col_cells_sa2:
-        sa2_l.append(cell_sa2.value.upper())
+        sa2_l.append(cell_sa2.value)
 
 # determine number of address before matching
 num_addresses = len(sa1_l) + len(sa2_l)
@@ -38,11 +42,42 @@ for addr_1 in sa1_l:
     # populate a list of matching addr_match objects for some address in list 1
     matches = []
     # iterate through every address in list 2
+    mod_addr_1 = ''
+    mod_addr_2 = ''
     for addr_2 in sa2_l:
+        # apply any transformations to addresses
+        # strip all commas away from the address
+        mod_addr_1 = addr_1.replace(',', '').upper()
+        mod_addr_2 = addr_2.replace(',', '').upper()
+        # remove the word 'flat'
+        mod_addr_1 = mod_addr_1.replace('FLAT', '')
+        mod_addr_2 = mod_addr_2.replace('FLAT', '')
+        # remove the word 'apartment'
+        mod_addr_1 = mod_addr_1.replace('APARTMENT', '')
+        mod_addr_2 = mod_addr_2.replace('APARTMENT', '')
+        # strip() removes leading and trailing whitespace
+        mod_addr_1 = mod_addr_1.strip()
+        mod_addr_2 = mod_addr_2.strip()
         # determine fuzzy matching ratio
-        f_ratio = fuzz.ratio(addr_1, addr_2)
-        f_tkn_ratio = fuzz.token_sort_ratio(addr_1, addr_2)
-        if f_tkn_ratio >= 75:
+        f_ratio = fuzz.ratio(mod_addr_1, mod_addr_2)
+        f_tkn_ratio = fuzz.token_sort_ratio(mod_addr_1, mod_addr_2)
+
+        # If the first part of the address is a number, check if they both match
+        # E.g 1, Charter House and 2, Charter House should not match!
+        # At this point, the Address has been stripped of commas and is ^case
+        # And the word Flat has been removed
+        # get first part of the address
+        addr_num1 = mod_addr_1.split(' ')[0]
+        addr_num2 = mod_addr_2.split(' ')[0]
+        # check if the first parts of the address are both numeric
+        if (addr_num1.isnumeric()) and (addr_num2.isnumeric()):
+            if addr_num1 != addr_num2:
+                # if the nums dont match reduce their ratio 'score' by 20
+                f_tkn_ratio -= 20
+                f_ratio -= 20
+
+        # check if the address is a potential match
+        if (f_tkn_ratio >= 73) and (f_ratio >= 63):
             # append the potential match to the matches list
             matches.append(addr_match(addr_2, f_ratio, f_tkn_ratio))
 
@@ -51,16 +86,15 @@ for addr_1 in sa1_l:
     if len(matches) > 0:
         match = matches[0]
         for addr in matches:
-            if addr.f_ratio > match.f_ratio:
+            if addr.f_tkn_ratio > match.f_tkn_ratio:
                 match = addr
         # now append the match and addr_1 to matched_addr list
         print("Found a match\n  ", addr_1, "||", match.addr, "\n\tratio: ",
             match.f_ratio, "tkn ratio: ", match.f_tkn_ratio)
-        # TODO: create an object storing the address and its matching address 
+        # TODO: create an object storing the address and its matching address
         matched_addr.append(match.addr)
         matched_addr.append(addr_1)
         # remove the matching address from sa2_l
-        sa2_l.remove(match.addr)
 
 print("# Σ Addresses:", num_addresses)
 print("matched", len(matched_addr), "addresses")
